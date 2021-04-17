848177f863590cec558f88e97d5284d674367a2c
Einar Gednochsson <pete80pro.pb@gmail.com>
2021-04-17 13:58:00 +0200

    sm8150-common: Remove duplicated Permission and add April Redfin FP

--- aosp_zippo.mk
+++ aosp_zippo.mk
@@ -9,7 +9,6 @@ $(call inherit-product, device/lenovo/zippo/device.mk)
 # Inherit some common Derpfest stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-EXTRA_FOD_ANIMATIONS := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := true
 CUSTOM_BUILD_TYPE := Official
@@ -21,10 +20,10 @@ PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo Z6 Pro
 PRODUCT_NAME := aosp_zippo
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210305.006 7119741 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys" \
     PRODUCT_NAME="zippo" \
     TARGET_DEVICE="zippo"
 
